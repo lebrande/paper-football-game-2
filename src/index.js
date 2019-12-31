95d49edbef556f83d1637d6f3703caad1bc592c3
Jakub Pusiak <jakub.pusiak@gmail.com>
2019-12-31 17:36:42 +0100

    Use parcel.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import GameField from './lib/GameField';
+
 const canvas = document.getElementById('game_screen');
 const ctx = canvas.getContext('2d');
+new GameField();
 
+ctx.fillStyle = '#F00';
+ctx.fillRect(20, 20, 100, 100);
+
+ctx.clearRect(0, 0, 800, 600);
+
+ctx.fillStyle = '#F0F';
+ctx.fillRect(200, 400, 50, 50);
---
+++ src/lib/GameField.js
@@ -0,0 +1,5 @@
+export default class GameField {
+  constructor() {
+    console.log('GameField')
+  }
+}
